693e9adf2c573d7729b3668017fe751471c981ea
Faris Saleh Ahmad <faris.ahmad@terracap.df.gov.br>
2019-12-03 17:52:24 -0300

    Adição de código baseados na vídeo aula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,38 @@
+import { StorageService } from './../services/storage.service';
+import { AuthService } from './../services/auth.service';
+import { CategoriaService } from './../services/domain/categoria.service';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage,
-    ListPage
+    MyApp
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,//modulo para requests http
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CategoriaService,//Uma instancia desta classe servirá para o projeto todo
+    ErrorInterceptorProvider,
+    AuthService,
+    StorageService
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
+import { AuthService } from './../../services/auth.service';
+import { CredenciaisDTO } from './../../models/credencias.dto';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 
-@Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+@IonicPage()//Indica que essa classe é um página e pode se referências entere aspas 'pagina'
+//Permite usar a classe pelo nome sem import para o lazy load
+@Component({//Decorator que permite que classe seja um controlador
+  selector: 'page-home',//nome atribuido
+  templateUrl: 'home.html'//Referênicas ao qual arquivo essa class controla
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  credenciais : CredenciaisDTO = {//Será feito um bind com html e por isso tem q começar vazio
+    email: "",
+    senha: ""
+  }
 
+  //Ionic e Angular para injetar uma intancia de objeto em uma classe
+  // basta decarar essse objeto como parámetro do construtor
+  constructor(
+    public navCtrl: NavController,
+    public menu: MenuController,
+    public auth: AuthService) {//Injeção de Dependencia
+
+  }
+  ionViewWillEnter(){
+    this.menu.swipeEnable(false)
+    this.auth.logout();//toda vez que a home for carregada será limpada os dados do usuário do storage
+  }
+
+  ionViewDidLeave(){
+    this.menu.swipeEnable(true)
+  }
+
+  login(){//sem nda entende-se q o método é público
+    //Todo elemente de uma classe tem q ser precedidio pela palavra this
+    //localStorage (HTML 5), armazenamento local, meio de pares (chave, vaor) string set/get/remove Item
+    this.auth.authenticate(this.credenciais)
+    .subscribe(response => {
+      this.auth.successfulLogin(response.headers.get('Authorization'));
+      // this.navCtrl.push('CategoriasPage') // Empilha uma página em cima da outra, fila de páginas
+      this.navCtrl.setRoot('CategoriasPage') // não empilha
+      },error => {});
   }
 
 }
